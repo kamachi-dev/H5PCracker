1a1bea548ae906a8cc9898a1f6c7eccebe9eab72
Machi <Angelo.kamachi03@gmail.com>
2025-07-08 17:50:11 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import JSZip from "jszip";
+
+function escapeHTML(str) {
+    return String(str)
+        .replace(/<[^>]*>/g, "")
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#39;");
+}
+
+function parseContent(json) {
+    let out = '';
+    let chapterCount = 0
+    for (const chapter of json.chapters) {
+        let questions = chapter.params.content[0].content.params.questions;
+        if (!questions) continue;
+
+        chapterCount++;
+        out += `<h1>Chapter ${escapeHTML(chapterCount)}</h1>`
+
+        for (const question of questions) {
+            let questiontxt = question.params.question;
+            if (questiontxt) {
+                out += `<h3>${escapeHTML(questiontxt)}</h3>`;
+
+                const answers = question.params.answers;
+                for (const answer of answers) {
+                    let answertxt = answer.text;
+                    const color = (answer.correct) ? 'green' : 'red';
+                    out += `<p class=${color}>${escapeHTML(answertxt)}</p>`;
+                }
+            }
+        }
+    }
+
+    document.getElementById('output').innerHTML = out;
+}
+
+
+document.getElementById('h5pFile').addEventListener('change', async (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const output = document.getElementById('output');
+
+    try {
+        const arrayBuffer = await file.arrayBuffer();
+        const zip = await JSZip.loadAsync(arrayBuffer);
+
+        const targetPath = "content/content.json";
+
+        if (zip.files[targetPath]) {
+            const text = await zip.files[targetPath].async("text");
+            const json = JSON.parse(text);
+            parseContent(json)
+        } else {
+            output.textContent = `⚠️ File not found: ${targetPath}`;
+        }
+
+    } catch (err) {
+        console.error(err);
+        output.textContent = "Error reading H5P file: " + err.message;
+    }
+});
